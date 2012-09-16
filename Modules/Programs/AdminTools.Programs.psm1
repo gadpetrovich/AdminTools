b99301fd6ed334ab11dd2652ebe22e738e2890c8
Pavel Sharapov <subber@yandex.ru>
2012-09-16 22:59:07 +0400

    Добавил возомжность нахождения 32-х разрядных приложений на 64-х битных системах.

--- Modules/Programs/AdminTools.Programs.psm1
+++ Modules/Programs/AdminTools.Programs.psm1
@@ -66,42 +66,49 @@ function Get-Program
 		[string]$AppMatch = ""
 	)            
 
-	begin {            
-		$UninstallRegKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"             
+	begin {
+		$HKLM   = [microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computer)
+		$UninstallRegKeys=@("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall")
+		if ($HKLM.OpenSubKey("SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall") -ne $null) {
+			$UninstallRegKeys += "SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+		}
 	}            
 
 	process {            
 		foreach($Computer in $ComputerName) {            
 			Write-Verbose "Working on $Computer"            
 			if(Test-Connection -ComputerName $Computer -Count 1 -ea 0) {            
-				$HKLM   = [microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computer)            
-				$UninstallRef  = $HKLM.OpenSubKey($UninstallRegKey)            
-				$Applications = $UninstallRef.GetSubKeyNames()            
+				
+				foreach ($UninstallRegKey in $UninstallRegKeys)
+				{
+					$UninstallRef  = $HKLM.OpenSubKey($UninstallRegKey)            
+					$Applications = $UninstallRef.GetSubKeyNames()            
 
-				foreach ($App in $Applications) {            
-					$AppRegistryKey  = $UninstallRegKey + "\\" + $App            
-					$AppDetails   = $HKLM.OpenSubKey($AppRegistryKey)            
-					
-					$AppDisplayName  = $($AppDetails.GetValue("DisplayName"))    
-					if($PSCmdlet.ParameterSetName -eq "Match" -and $AppDisplayName -notmatch $AppMatch ) { continue; }
-					if($PSCmdlet.ParameterSetName -eq "Name" -and $AppName -notcontains $AppDisplayName ) { continue; }
-					
-					$AppVersion   = $($AppDetails.GetValue("DisplayVersion"))            
-					$AppPublisher  = $($AppDetails.GetValue("Publisher"))            
-					$AppInstalledDate = $($AppDetails.GetValue("InstallDate"))            
-					$AppUninstall  = $($AppDetails.GetValue("UninstallString"))            
-					$AppGUID   = $App            
-					if(!$AppDisplayName) { continue }            
-					$OutputObj = New-Object -TypeName PSobject             
-					$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer.ToUpper()            
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $AppDisplayName            
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppVersion -Value $AppVersion            
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppVendor -Value $AppPublisher            
-					$OutputObj | Add-Member -MemberType NoteProperty -Name InstalledDate -Value $AppInstalledDate            
-					$OutputObj | Add-Member -MemberType NoteProperty -Name UninstallKey -Value $AppUninstall            
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $AppGUID            
-					$OutputObj# | Select ComputerName, DriveName            
-				}            
+					foreach ($App in $Applications) {            
+						$AppRegistryKey  = $UninstallRegKey + "\\" + $App            
+						$AppDetails   = $HKLM.OpenSubKey($AppRegistryKey)            
+						
+						$AppDisplayName  = $($AppDetails.GetValue("DisplayName"))    
+						if($PSCmdlet.ParameterSetName -eq "Match" -and $AppDisplayName -notmatch $AppMatch ) { continue; }
+						if($PSCmdlet.ParameterSetName -eq "Name" -and $AppName -notcontains $AppDisplayName ) { continue; }
+						
+						$AppVersion   = $($AppDetails.GetValue("DisplayVersion"))            
+						$AppPublisher  = $($AppDetails.GetValue("Publisher"))            
+						$AppInstalledDate = $($AppDetails.GetValue("InstallDate"))            
+						$AppUninstall  = $($AppDetails.GetValue("UninstallString"))            
+						$AppGUID   = $App            
+						if(!$AppDisplayName) { continue }            
+						$OutputObj = New-Object -TypeName PSobject             
+						$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer.ToUpper()            
+						$OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $AppDisplayName            
+						$OutputObj | Add-Member -MemberType NoteProperty -Name AppVersion -Value $AppVersion            
+						$OutputObj | Add-Member -MemberType NoteProperty -Name AppVendor -Value $AppPublisher            
+						$OutputObj | Add-Member -MemberType NoteProperty -Name InstalledDate -Value $AppInstalledDate            
+						$OutputObj | Add-Member -MemberType NoteProperty -Name UninstallKey -Value $AppUninstall            
+						$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $AppGUID            
+						$OutputObj# | Select ComputerName, DriveName            
+					}
+				}				
 			}            
 		}            
 	}            
