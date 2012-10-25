34b677f82b2a480bdad36b732bbb16e3054f67cd
pavel_sharapov <gadpetrovich@gmail.com>
2012-10-25 11:31:16 +0400

    Исправил ошибки в Programs

--- Modules/Programs/AdminTools.Programs.psm1
+++ Modules/Programs/AdminTools.Programs.psm1
@@ -228,31 +228,33 @@ function Uninstall-Program
 				}
 			}
 			
-			switch ($($returnvalue)){
-				-1 { $txt = "Canceled" }
-				0 { $txt = "Uninstallation command triggered successfully" }
-				2 { $txt = "You don't have sufficient permissions to trigger the command on $Computer" }
-				3 { $txt = "You don't have sufficient permissions to trigger the command on $Computer" }
-				8 { $txt = "An unknown error has occurred" }
-				9 { $txt = "Path Not Found" }
-				21 { $txt = "Invalid Parameter"}
-			}
 			Write-Verbose "Получаем список событий, связанных с удалением"
 			$events = @(Get-EventLog -computername $ComputerName -LogName Application -Source MsiInstaller -After $before_uninstall_date)
 			$event_message = @()
 			foreach($i in $events) { $event_message += $i.message }
-			
-			$OutputObj = New-Object -TypeName PSobject     
-			$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName
-			$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $AppGUID
-			$OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $appName
-			$OutputObj | Add-Member -MemberType NoteProperty -Name ReturnValue -Value $returnvalue
-			$OutputObj | Add-Member -MemberType NoteProperty -Name Text -Value $txt
-			$OutputObj | Add-Member -MemberType NoteProperty -Name EventMessage -Value $event_message
-			$OutputObj
 		} catch {
 			Write-Error $_
-		}	
+		}
+		switch ($($returnvalue)){
+			-1 { $txt = "Canceled" }
+			0 { $txt = "Uninstallation command triggered successfully" }
+			2 { $txt = "You don't have sufficient permissions to trigger the command on $Computer" }
+			3 { $txt = "You don't have sufficient permissions to trigger the command on $Computer" }
+			8 { $txt = "An unknown error has occurred" }
+			9 { $txt = "Path Not Found" }
+			21 { $txt = "Invalid Parameter"}
+		}
+		
+		
+		$OutputObj = New-Object -TypeName PSobject     
+		$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName
+		$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $AppGUID
+		$OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $appName
+		$OutputObj | Add-Member -MemberType NoteProperty -Name ReturnValue -Value $returnvalue
+		$OutputObj | Add-Member -MemberType NoteProperty -Name Text -Value $txt
+		$OutputObj | Add-Member -MemberType NoteProperty -Name EventMessage -Value $event_message
+		$OutputObj
+		
 	}
 	end {}
 }
@@ -340,14 +342,14 @@ function Install-Program()
 	begin {}
 	process {
 		try {
+			$temp_file = [System.IO.Path]::GetTempFileName()
+			
 			$currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() ) 
 			if (!$currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) 
 			{ 
 				throw "Для установки приложения требуются права администратора"
 			}
 		
-			$temp_file = [System.IO.Path]::GetTempFileName()
-			
 			$file = Get-Item $ProgSource
 			$params = ""
 			
@@ -380,7 +382,7 @@ function Install-Program()
 			$msiserver = Get-Service -ComputerName $ComputerName -Name msiserver
 			if ($msiserver.Status -ne "Running") { $msiserver.Start() }
 			while (@(Get-Process msiexec -ComputerName $ComputerName).Count -gt 1){ Sleep 2 }
-			
+		
 			Write-Verbose "Получаем список программ"
 			$after_install_state = Get-Program -ComputerName $ComputerName
 			$diff = @(diff $before_install_state $after_install_state -Property AppName, AppVersion, AppVendor, AppGUID | ? { $_.SideIndicator -eq "=>" } )
@@ -390,36 +392,42 @@ function Install-Program()
 			$event_message = @()
 			foreach($i in $events) { $event_message += $i.message }
 			
-			if ($diff) {
-				foreach( $i in $diff) {
-					$OutputObj = New-Object -TypeName PSobject             
-					$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName
-					$OutputObj | Add-Member -MemberType NoteProperty -Name ProgSource -Value $ProgSource
-					$OutputObj | Add-Member -MemberType NoteProperty -Name ReturnValue -Value $exit_code
-					$OutputObj | Add-Member -MemberType NoteProperty -Name EventMessage -Value $event_message
-					$OutputObj | Add-Member -MemberType NoteProperty -Name OutputData -Value (cat $temp_file)
-					
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $i.AppName
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppVersion -Value $i.AppVersion
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppVendor -Value $i.AppVendor
-					$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $i.AppGUID
-					$OutputObj
-				}
-			} else {
+		} catch {
+			$exit_code = -1
+			Write-Error $_
+		}
+		
+		
+		if ($diff) {
+			foreach( $i in $diff) {
 				$OutputObj = New-Object -TypeName PSobject             
 				$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName
 				$OutputObj | Add-Member -MemberType NoteProperty -Name ProgSource -Value $ProgSource
 				$OutputObj | Add-Member -MemberType NoteProperty -Name ReturnValue -Value $exit_code
 				$OutputObj | Add-Member -MemberType NoteProperty -Name EventMessage -Value $event_message
-				$OutputObj | Add-Member -MemberType NoteProperty -Name OutputData -Value (cat $temp_file)
+				if (Test-Path $temp_file) {
+					$OutputObj | Add-Member -MemberType NoteProperty -Name OutputData -Value (cat $temp_file)
+				}
+				
+				$OutputObj | Add-Member -MemberType NoteProperty -Name AppName -Value $i.AppName
+				$OutputObj | Add-Member -MemberType NoteProperty -Name AppVersion -Value $i.AppVersion
+				$OutputObj | Add-Member -MemberType NoteProperty -Name AppVendor -Value $i.AppVendor
+				$OutputObj | Add-Member -MemberType NoteProperty -Name AppGUID -Value $i.AppGUID
 				$OutputObj
 			}
-			rm -Force $temp_file -ErrorAction SilentlyContinue
+		} else {
+			$OutputObj = New-Object -TypeName PSobject             
+			$OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ComputerName
+			$OutputObj | Add-Member -MemberType NoteProperty -Name ProgSource -Value $ProgSource
+			$OutputObj | Add-Member -MemberType NoteProperty -Name ReturnValue -Value $exit_code
+			$OutputObj | Add-Member -MemberType NoteProperty -Name EventMessage -Value $event_message
+			if (Test-Path $temp_file) {
+				$OutputObj | Add-Member -MemberType NoteProperty -Name OutputData -Value (cat $temp_file)
+			}
 			
-		} catch {
-			Write-Error $_
+			$OutputObj
 		}
-		
+		rm -Force $temp_file -ErrorAction SilentlyContinue
 	}
 	
 	end {}
